afeeaa649c661cad40d46f19a2e1dde096e42884
Mayank pandey <mpandeymp0@gmail.com>
2026-01-01 10:06:30 +0530

    blogs app and blog model

---
+++ app/admin.py
@@ -0,0 +1,25 @@
+from django.contrib import admin
+from .models import Category, Blog
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["category_name", "updated_at", "created_at"]
+    search_fields = ["category_name"]
+
+
+class BlogAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title",)}
+    list_display = (
+        "title",
+        "category",
+        "author",
+        "status",
+        "is_featured",
+        "created_at",
+    )
+    search_fields = ("id", "title", "category__category_name")
+    list_editable = ("status", "is_featured")
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Blog, BlogAdmin)
--- blog/settings.py
+++ blog/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-$igmhpn#ozi*%upixmd$dlodx4&px(dmn3hf0-n2q1h0990d-m"
+SECRET_KEY = config("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "app",
 ]
 
 MIDDLEWARE = [
@@ -117,3 +119,7 @@ USE_TZ = True
 STATIC_URL = "static/"
 STATIC_ROOT = BASE_DIR / "static"
 STATICFILES_DIRS = ["blog/static"]
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media"
