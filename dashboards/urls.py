10f546c573049a9442e3aaf664a990481f7f4b0c
Mayank pandey <mpandeymp0@gmail.com>
2026-02-13 15:05:20 +0530

    Posts CRUD operation

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -3,8 +3,14 @@ from . import views
 
 urlpatterns = [
     path("", views.dashboard, name="dashboard"),
+    # Categorie operations
     path("categories/", views.categories, name="categories"),
-    path("categories/add/", views.add_category, name="add_category"),
+    path("categories/add_category/", views.add_category, name="add_category"),
     path("categories/edit/<int:pk>/", views.edit_category, name="edit_category"),
     path("categories/delete/<int:pk>/", views.delete_category, name="delete_category"),
+    # Posts operations
+    path("posts/", views.posts, name="posts"),
+    path("posts/add_posts/", views.add_posts, name="add_posts"),
+    path("posts/edit/<int:pk>", views.edit_posts, name="edit_posts"),
+    path("posts/delete/<int:pk>", views.delete_posts, name="delete_posts"),
 ]
